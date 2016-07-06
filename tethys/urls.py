cacebbb994720897a98bef659cc21ec85690c0d9
s-herbert <sherbert@ucsd.edu>
2016-07-05 17:56:02 -0700

    Added ajax/jquery for refresh button that works!! updated view for it

--- tethys/urls.py
+++ tethys/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.container_list, name='container_list'),
 	url(r'^collection/(?P<name>\w+)/$', views.container_detail, name='container_detail'),
+	url(r'^refresh_container/$',views.refresh_container, name='refresh_container')
 ]
--- tethys/views.py
+++ tethys/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 from .models import TethysServer,Container
+
+import json
+
 
 # Create your views here.
 def container_list(request):
@@ -9,8 +13,26 @@ def container_list(request):
 					}
 	return render(request, 'tethys/container_list.html', content_dict)
 	
-def refresh_container(container):
-	container.refresh()
+def refresh_container(request):
+	if request.method == 'GET':
+		collection = request.GET.get('the_collection')
+		response_data = {}
+
+		container = Container.objects.get(name=collection)
+		container.refresh()
+
+		response_data['result'] = 'refresh successful!'
+		response_data['count'] = container.count
+
+		return HttpResponse(
+			json.dumps(response_data),
+			content_type="application/json"
+		)
+	else:
+		return HttpResponse(
+			json.dumps({"nothing to see": "this isn't happening"}),
+			content_type="application/json"
+		)
 	
 def container_detail(request,name):
 	container = get_object_or_404(Container,name=name)
