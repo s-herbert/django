d02dd84d8aed33a2b4be1a7f6e87c59bb8633e86
s-herbert <sherbert@ucsd.edu>
2016-07-06 03:19:15 -0700

    added static js/css/fonts..got wsgi.py set up and apache going!!

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -7,10 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
+import os,sys
 
-from django.core.wsgi import get_wsgi_application
+
+apache_configuration= os.path.dirname(__file__)
+project = os.path.dirname(apache_configuration)
+workspace = os.path.dirname(project)
+sys.path.append(workspace)
+sys.path.append(project)
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
