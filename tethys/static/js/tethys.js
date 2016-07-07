ff1abd0160c8ddb4821f33bbd9bf95dc07e8e9df
s-herbert <sherbert@ucsd.edu>
2016-07-06 22:15:39 -0700

    added bootstrap selection, as well as ajax to filter what's displayed!

--- tethys/static/js/tethys.js
+++ tethys/static/js/tethys.js
@@ -1,6 +1,48 @@
-
+//Function from http://www.lessanvaezi.com/filter-select-list-options/
+//filters a selection box based on input from a text box!
+jQuery.fn.filterByText = function(textbox, selectSingleMatch) {
+	return this.each(function() {
+		var select = this;
+		var options = [];
+		$(select).find('option').each(function() {
+			options.push({value: $(this).val(), text: $(this).text()});
+		});
+		$(select).data('options', options);
+		$(textbox).bind('change keyup', function() {
+			var options = $(select).empty().data('options');
+			var search = $(this).val().trim();
+			var regex = new RegExp(search,"gi");
+		  
+			$.each(options, function(i) {
+				var option = options[i];
+				if(option.text.match(regex) !== null) {
+					$(select).append(
+					   $('<option>').text(option.text).val(option.value)
+					);
+				}
+			});
+			if (selectSingleMatch === true && $(select).children().length === 1) {
+				$(select).children().get(0).selected = true;
+			}
+		});            
+	});
+};
 
 $(function() {
+	
+
+
+    $(function() {
+        $('#select').filterByText($('#text-filter'), false);
+//      $("select option").click(function(){
+  //      alert(1);
+    //  });
+    });
+	
+	
+	
+	
+	
 
 	//refresh container info
 	$('.refresh-btn').click(function(event){
